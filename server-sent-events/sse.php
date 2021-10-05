c51f0f5d0eedfaf4a7e6506c01dbf27fbe7af2cb
Ezekiel Victor <zekevictor@gmail.com>
2021-10-04 23:39:44 -0700

    simplify code and make it easier to comprehend

--- server-sent-events/sse.php
+++ server-sent-events/sse.php
@@ -2,36 +2,26 @@
 date_default_timezone_set("America/New_York");
 header("Content-Type: text/event-stream");
 
-$counter = rand(1, 10); // a random counter
-while (1) {
-// 1 is always true, so repeat the while loop forever (aka event-loop)
-
+// While connection not aborted, send a message randomly with 10% (0.1) probability
+for(
+  $pctChanceSendMsg = 0.1;
+  !connection_aborted();
+  $shouldSendMsg = rand() < $pctChanceSendMsg
+) {
   $curDate = date(DATE_ISO8601);
   echo "event: ping\n",
        'data: {"time": "' . $curDate . '"}', "\n\n";
 
-  // Send a simple message at random intervals.
-
-  $counter--;
-
-  if (!$counter) {
+  if ($shouldSendMsg) {
     echo 'data: This is a message at time ' . $curDate, "\n\n";
-    $counter = rand(1, 10); // reset random counter
   }
 
   // flush the output buffer and send echoed messages to the browser
-
   while (ob_get_level() > 0) {
     ob_end_flush();
   }
   flush();
 
-  // break the loop if the client aborted the connection (closed the page)
-  
-  if ( connection_aborted() ) break;
-
-  // sleep for 1 second before running the loop again
-  
+  // sleep for 1 second before running the loop again  
   sleep(1);
-
 }
